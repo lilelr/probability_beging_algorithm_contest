ffede74f30ed4110e6f4475aecbfeb569c19f0cf
lilelr <lilelr@163.com>
2016-09-24 13:51:47 +0800

    杨辉三角求解 结合二项式定理

--- probability_beging_algorithm_contest/main.cpp
+++ probability_beging_algorithm_contest/main.cpp
@@ -1,15 +1,15 @@
+////
+////  main.cpp
+////  probability_beging_algorithm_contest
+////
+////  Created by YuXiao on 9/24/16.
+////  Copyright © 2016 YuXiao. All rights reserved.
+////
 //
-//  main.cpp
-//  probability_beging_algorithm_contest
+//#include <iostream>
 //
-//  Created by YuXiao on 9/24/16.
-//  Copyright © 2016 YuXiao. All rights reserved.
-//
-
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
-}
+// int main(int argc, const char * argv[]) {
+//    // insert code here...
+//    std::cout << "Hello, World!\n";
+//    return 0;
+//}
---
+++ probability_beging_algorithm_contest/yang_hui_triangle.cpp
@@ -0,0 +1,42 @@
+//杨辉三角求解,O(n) 结合二项式定理
+
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <string>
+#include <vector>
+using namespace std;
+#define LL long long
+const int maxn = 100;
+int c[maxn];
+int n;
+
+int main() {
+  freopen("/Users/yuxiao/XcodeProject/probability_beging_algorithm_contest/"
+          "probability_beging_algorithm_contest/in",
+          "r", stdin);
+  freopen("/Users/yuxiao/XcodeProject/probability_beging_algorithm_contest/"
+          "probability_beging_algorithm_contest/out",
+          "w", stdout);
+
+  int T, t;
+  cin >> T;
+  t = 1;
+  while (T--) {
+    cin >> n;
+    c[0] = 1;
+    cout << c[0];
+    for (int i = 1; i <= n; i++) {
+      c[i] = c[i - 1] * (n - i + 1) / i;
+      cout << " " << c[i];
+    }
+    cout << endl;
+  }
+  return 0;
+}
