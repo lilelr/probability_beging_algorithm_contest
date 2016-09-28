2e80ca561f2338ea4c591b309c1901f4c76099a9
lilelr <lilelr@163.com>
2016-09-28 23:23:38 +0800

    //用递推法 计算排列组合，经典 Uva 12034

--- probability_beging_algorithm_contest/Critical Mass Uva580.cpp
+++ probability_beging_algorithm_contest/Critical Mass Uva580.cpp
@@ -1,39 +1,39 @@
-
-//用递推法 计算排列组合
-#include <algorithm>
-#include <cmath>
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
-#include <iostream>
-#include <map>
-#include <queue>
-#include <set>
-#include <string>
-#include <vector>
-using namespace std;
-#define LL long long
-const int maxn = 1000;
-
-int f[31], g[31];
-
-int main() {
-  //    freopen("", "r", stdin);
-  //    freopen("", "w", stdout);
-  f[0] = f[1] = f[2] = 0;
-  g[0] = 1, g[1] = 2, g[2] = 4;
-  for (int n = 3; n <= 30; n++) {
-    f[n] = 1 << (n - 3);
-    for (int i = 2; i <= n - 2; i++) {
-      f[n] += g[i - 2] * (1 << (n - i - 2));
-    }
-    g[n] = (1 << n) - f[n];
-  }
-  int n;
-  while (cin >> n && n) {
-    cout << f[n] << "\n";
-  }
-  return 0;
-
-  return 0;
-}
+//
+////用递推法 计算排列组合
+//#include <algorithm>
+//#include <cmath>
+//#include <cstdio>
+//#include <cstdlib>
+//#include <cstring>
+//#include <iostream>
+//#include <map>
+//#include <queue>
+//#include <set>
+//#include <string>
+//#include <vector>
+//using namespace std;
+//#define LL long long
+//const int maxn = 1000;
+//
+//int f[31], g[31];
+//
+//int main() {
+//  //    freopen("", "r", stdin);
+//  //    freopen("", "w", stdout);
+//  f[0] = f[1] = f[2] = 0;
+//  g[0] = 1, g[1] = 2, g[2] = 4;
+//  for (int n = 3; n <= 30; n++) {
+//    f[n] = 1 << (n - 3);
+//    for (int i = 2; i <= n - 2; i++) {
+//      f[n] += g[i - 2] * (1 << (n - i - 2));
+//    }
+//    g[n] = (1 << n) - f[n];
+//  }
+//  int n;
+//  while (cin >> n && n) {
+//    cout << f[n] << "\n";
+//  }
+//  return 0;
+//
+//  return 0;
+//}
---
+++ probability_beging_algorithm_contest/Uva12304 Race.cpp
@@ -0,0 +1,51 @@
+
+//用递推法 计算排列组合，经典
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <string>
+#include <vector>
+using namespace std;
+#define LL long long
+const int maxn = 1000;
+const int MOD = 10056;
+int C[maxn + 1][maxn + 1], f[maxn + 1];
+
+// 递推出所有组合数
+void init() {
+  for (int i = 0; i <= maxn; i++) {
+    C[i][0] = C[i][i] = 1;
+    for (int j = 1; j < i; j++) {
+      C[i][j] = (C[i - 1][j] + C[i - 1][j - 1]) % MOD;
+    }
+  }
+}
+
+int main() {
+  //    freopen("", "r", stdin);
+  //    freopen("", "w", stdout);
+  init();
+  f[0] = 1;
+  for (int i = 1; i <= maxn; i++) {
+    f[i] = 0;
+    for (int j = 1; j <= i; j++) {
+      f[i] = (f[i] + C[i][j] * f[i - j]) % MOD;
+    }
+  }
+
+  int T, n;
+  scanf("%d", &T);
+
+  for (int kcase = 1; kcase <= T; kcase++) {
+    scanf("%d", &n);
+    printf("Case %d: %d\n", kcase, f[n]);
+  }
+
+  return 0;
+}
