afd1118acc9c45b4154a93a75d45768b31b05820
lilelr <lilelr@163.com>
2016-10-15 11:05:34 +0800

    n

--- probability_beging_algorithm_contest/Uva 1638 Pole Arrangement.cpp
+++ probability_beging_algorithm_contest/Uva 1638 Pole Arrangement.cpp
@@ -1,47 +1,47 @@
-// // UVa1638/LA6117 Pole Arrangement 杆子的插放 经典的模拟 + 递推法
-// https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=850&page=show_problem&problem=4513
-#include <algorithm>
-#include <cmath>
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
-#include <iostream>
-#include <map>
-#include <queue>
-#include <set>
-#include <string>
-#include <vector>
-using namespace std;
-#define LL long long
-const int maxn = 20;
-
-long long d[maxn + 1][maxn + 1][maxn + 1];
-
-int main() {
-  //  freopen("", "r", stdin);
-  //  freopen("", "w", stdout);
-
-  d[1][1][1] = 1;
-
-  for (int i = 2; i <= maxn; i++) {
-    for (int j = 1; j <= i; j++) {
-      for (int k = 1; k <= i; k++) {
-        d[i][j][k] = d[i - 1][j][k] * (i - 2);
-        if (j > 1) {
-          d[i][j][k] +=
-              d[i - 1][j - 1][k]; // i-1 状态 左边能看到j-1根，右边能看到k根
-        }
-        if (k > 1) {
-          d[i][j][k] += d[i - 1][j][k - 1];
-        }
-      }
-    }
-  }
-  int T, n, L, R;
-  cin >> T;
-  while (T--) {
-    cin >> n >> L >> R;
-    cout << d[n][L][R] << "\n";
-  }
-  return 0;
-}
+//// // UVa1638/LA6117 Pole Arrangement 杆子的插放 经典的模拟 + 递推法
+//// https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=850&page=show_problem&problem=4513
+//#include <algorithm>
+//#include <cmath>
+//#include <cstdio>
+//#include <cstdlib>
+//#include <cstring>
+//#include <iostream>
+//#include <map>
+//#include <queue>
+//#include <set>
+//#include <string>
+//#include <vector>
+//using namespace std;
+//#define LL long long
+//const int maxn = 20;
+//
+//long long d[maxn + 1][maxn + 1][maxn + 1];
+//
+//int main() {
+//  //  freopen("", "r", stdin);
+//  //  freopen("", "w", stdout);
+//
+//  d[1][1][1] = 1;
+//
+//  for (int i = 2; i <= maxn; i++) {
+//    for (int j = 1; j <= i; j++) {
+//      for (int k = 1; k <= i; k++) {
+//        d[i][j][k] = d[i - 1][j][k] * (i - 2);
+//        if (j > 1) {
+//          d[i][j][k] +=
+//              d[i - 1][j - 1][k]; // i-1 状态 左边能看到j-1根，右边能看到k根
+//        }
+//        if (k > 1) {
+//          d[i][j][k] += d[i - 1][j][k - 1];
+//        }
+//      }
+//    }
+//  }
+//  int T, n, L, R;
+//  cin >> T;
+//  while (T--) {
+//    cin >> n >> L >> R;
+//    cout << d[n][L][R] << "\n";
+//  }
+//  return 0;
+//}
--- probability_beging_algorithm_contest/Uva12230 Crossing Rivers.cpp
+++ probability_beging_algorithm_contest/Uva12230 Crossing Rivers.cpp
@@ -1,9 +1,20 @@
-// 均匀分布 数学期望
+////均匀分布 数学期望
+////全期望公式，把所有情况不重复、不遗漏分成若干类，每类计算数学期望，然后按照每类的概率加权求和
 //// UVa12230 Crossing Rivers
 //// Rujia Liu
 //#include <cstdio>
 //int main() {
-//  int n, D, p, L, v, kase = 0;
+//  //  int n, D, p, L, v, kase = 0;
+//  //  while (scanf("%d%d", &n, &D) == 2 && D) {
+//  //    double ans = 0;
+//  //    while (n--) {
+//  //      scanf("%d%d%d", &p, &L, &v);
+//  //      D -= L;
+//  //      ans += 2.0 * L / v;
+//  //    }
+//  //    printf("Case %d: %.3lf\n\n", ++kase, ans + D);
+//  //  }
+//  int n, D, p, L, v, kcase = 0;
 //  while (scanf("%d%d", &n, &D) == 2 && D) {
 //    double ans = 0;
 //    while (n--) {
@@ -11,7 +22,7 @@
 //      D -= L;
 //      ans += 2.0 * L / v;
 //    }
-//    printf("Case %d: %.3lf\n\n", ++kase, ans + D);
+//    printf("Case %d: %.3lf\n\n", ++kcase, ans + D);
 //  }
 //  return 0;
 //}
